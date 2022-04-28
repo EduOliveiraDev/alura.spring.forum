c12ba8ff463c96c5ecb809aeb8eec89bec1c3619
Eduardo <eduquimicaufu@gmail.com>
2022-04-28 14:59:11 -0300

    Autorização e Autenticação

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$6POO7eXCmeVDIOhSl6YbTuMLjf61XE5ahlJF5ci8lkBs9YR8Q5B1C');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
